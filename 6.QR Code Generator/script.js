dd325e734d6f83954a3c490e047807ab05743634
iamaayush-git <aayushbhattaraia6@gmail.com>
2023-12-19 18:59:44 +0545

    updated

--- 6.QR Code Generator/script.js
+++ 6.QR Code Generator/script.js
@@ -2,7 +2,6 @@ let qrImg = document.querySelector('#qrImg');
 let input = document.querySelector('#input');
 let btn = document.querySelector('#btn');
 
-
 btn.addEventListener('click',()=>{
     if(input.value){
         fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`).then((data)=>{
