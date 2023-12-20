e402dbd682b4a9592ccece78c9e12d940c5c00e9
iamaayush-git <aayushbhattaraia6@gmail.com>
2023-12-20 18:43:21 +0545

    added Enter feature

--- 6.QR Code Generator/script.js
+++ 6.QR Code Generator/script.js
@@ -1,11 +1,16 @@
 let qrImg = document.querySelector('#qrImg');
 let input = document.querySelector('#input');
 let btn = document.querySelector('#btn');
-
-btn.addEventListener('click',()=>{
-    if(input.value){
-        fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`).then((data)=>{
-          qrImg.src = data.url;
-        });
-    }
+function generate(){
+  if(input.value){
+    fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input.value}`).then((data)=>{
+      qrImg.src = data.url;
+    });
+}
+}
+btn.addEventListener('click',generate);
+document.addEventListener('keydown',(e)=>{
+  if(e.key === "Enter"){
+    generate();
+  }
 })
